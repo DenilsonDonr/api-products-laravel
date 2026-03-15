448a0d9c5fee9349626906c5ce38023a295f32a6
DenilsonDonr <denilsontrillo@gmail.com>
2026-03-15 23:01:38 +0000

    feat: add chema products

--- database/migrations/2026_03_15_224628_create_products_table.php
+++ database/migrations/2026_03_15_224628_create_products_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            
+            // Fk to categories
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+
+            $table->string('name', 255)->nullable(false);
+            $table->string('slug', 255)->unique()->nullable(false);
+            $table->text('description')->nullable();
+            $table->string('sku', 100)->unique()->nullable(false);
+            $table->decimal('price', 10, 2)->nullable(false);
+            $table->integer('stock')->default(0);
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
