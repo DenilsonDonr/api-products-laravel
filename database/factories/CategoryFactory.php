4417b7885564564b3b8bf2be5578607eddd0d358
DenilsonDonr <denilsontrillo@gmail.com>
2026-03-15 23:22:20 +0000

    feat(category-factory): define default attributes for category factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends Factory<Category>
@@ -18,7 +19,22 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->unique()->randomElement(
+                [
+                    'Electronics',
+                    'Books',
+                    'Clothing',
+                    'Home & Kitchen',
+                    'Sports & Outdoors',
+                    'Toys & Games',
+                    'Health & Personal Care',
+                    'Automotive',
+                    'Beauty & Grooming',
+                    'Office Supplies'
+                ]
+            ),
+            'slug' => fn (array $attrs) => Str::slug($attrs['name']), // generar un slug a partir del nombre de la categoría
+            'description' => fake()->sentence(), // generar una frase aleatoria para la descripción
         ];
     }
 }
