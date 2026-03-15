92819bc985233a0862b8022001131bc1d334f1ad
DenilsonDonr <denilsontrillo@gmail.com>
2026-03-15 23:39:24 +0000

    feat(seeders): enhance CategorySeeder and ProductSeeder for improved data seeding

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,13 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Toma las categorías existentes y crea productos asociados a cada categoría
+        $categories = Category::all();
+
+        $categories->each(function (Category $category) {
+            Product::factory()
+                ->count(rand(3, 8))
+                ->create([ 'category_id' => $category->id ]);
+        });
     }
 }
