77bea50e55746209e1d6f0d6c2ea657650b644af
DenilsonDonr <denilsontrillo@gmail.com>
2026-03-16 15:52:34 +0000

    feat(category-resource): include is_active attribute in CategoryResource transformation

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -21,6 +21,7 @@ class CategoryResource extends JsonResource
             'description' => $this->description,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'is_active' => $this->is_active,
         ];
     }
 }
