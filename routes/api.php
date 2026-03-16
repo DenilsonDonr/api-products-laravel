09f44a82aa2b579e35a679386feb8701b092fabf
DenilsonDonr <denilsontrillo@gmail.com>
2026-03-16 19:10:38 +0000

    feat(api): add POST route for category creation

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::get('categories', [CategoryController::class, 'index']);
+    Route::post('categories', [CategoryController::class, 'store']);
 });
