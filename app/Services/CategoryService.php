6f979efad57f4db6bb719eb482ad4859ba23849e
DenilsonDonr <denilsontrillo@gmail.com>
2026-03-16 19:10:26 +0000

    feat(category-service): add store method for saving new categories

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\DTOs\CategoryDTO;
 use App\Repositories\Contracts\CategoryRepositoryInterface;
 
 class CategoryService
@@ -13,5 +14,10 @@ class CategoryService
     public function all(int $perPage = 15, bool $isActive = true)
     {
         return $this->categoryRepository->all($perPage, $isActive);
+    }
+
+    public function store(CategoryDTO $dto)
+    {
+        return $this->categoryRepository->store($dto);
     }
 }
