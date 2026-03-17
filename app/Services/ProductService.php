feaa9aac3b19a1f27af224d34464921d6f5c1efa
DenilsonDonr <denilsontrillo@gmail.com>
2026-03-17 00:25:14 +0000

    feat(product): add store functionality with validation for product creation and corresponding tests

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\DTOs\ProductDTO;
+use App\Models\Product;
 use App\Repositories\Contracts\ProductRepositoryInterface;
 
 class ProductService
@@ -13,5 +15,10 @@ class ProductService
     public function all(int $perPage = 15, bool $isActive = true)
     {
         return $this->productRepository->all($perPage, $isActive);
+    }
+
+    public function store(ProductDTO $dto): Product
+    {
+        return $this->productRepository->store($dto);
     }
 }
