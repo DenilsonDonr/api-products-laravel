385f3f76c93f54f0378a3bfa8e6f3ec1c3439898
DenilsonDonr <denilsontrillo@gmail.com>
2026-03-19 06:25:10 +0000

    feat(api): add route for updating products by ID

--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,5 @@ Route::prefix('v1')->group(function () {
     Route::get('products', [ProductController::class, 'index']);
     Route::post('products', [ProductController::class, 'store']);
     Route::get('products/{id}', [ProductController::class, 'show']);
+    Route::put('products/{id}', [ProductController::class, 'update']);
 });
